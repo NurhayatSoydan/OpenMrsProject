fcbcb9d97998f25ddf1be87eb3d90ebac88f4690
Serkan <ozsahinserkan@gmail.com>
2023-09-10 14:54:49 +0300

    patientMerge kontrolleri yapıldı

--- src/SerkanOzsahin.java
+++ src/SerkanOzsahin.java
@@ -1,6 +1,7 @@
 import Utility.BaseDriver;
 import Utility.MyFunc;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
@@ -13,6 +14,8 @@ public class SerkanOzsahin extends BaseDriver {
     String validPassword = "Admin123";
     String invalidId = "Admin1";
     String invalidPassword = "Admin1234";
+    String patient1ID = "100NRM";
+    String patient2ID = "100NTK";
 
     @Test(priority = 1)
     public void locationNull() { // Location not selected
@@ -114,18 +117,29 @@ public class SerkanOzsahin extends BaseDriver {
     @Test
     public void patientMerge() {
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+        SerkanOzsahin_POM elements = new SerkanOzsahin_POM();
+        driver.get("https://openmrs.org/");
+        MyFunc.Bekle(1);
+        elements.demoBtn.click();
+        wait.until(ExpectedConditions.urlToBe("https://openmrs.org/demo/"));
+        elements.openMRS2.click();
+        MyFunc.Bekle(2);
+        elements.openMRS2Demo.click();
+        wait.until(ExpectedConditions.urlToBe("https://demo.openmrs.org/openmrs/login.htm"));
+        elements.userName.sendKeys(validId);
+        elements.password.sendKeys(validPassword);
+        elements.pharmacyBtn.click();
+        elements.loginBtn.click();
+        elements.dataMBtn.click();
+        elements.mergePBtn.click();
+        elements.patient1.sendKeys(patient1ID);
+        elements.patient2.sendKeys(patient2ID + Keys.ENTER);
+        wait.until(ExpectedConditions.elementToBeClickable(elements.confirmBtn));
+        elements.confirmBtn.click();
+        wait.until(ExpectedConditions.visibilityOf(elements.warningText));
+        Assert.assertTrue(elements.warningText.getText().contains("Merging cannot"));
+        elements.firstPatient.click();
+        elements.confirmBtn.click();
+        wait.until(ExpectedConditions.urlContains("patientDashboard"));
     }
 }
