bdbe033d72a960d7f5cc2fe94efcc28c80267799
NurhayatSoydan <141257552+NurhayatSoydan@users.noreply.github.com>
2023-09-14 14:33:00 +0300

    hasta silme eklendi

--- src/OpenMrsMain.java
+++ src/OpenMrsMain.java
@@ -482,4 +482,79 @@ public class OpenMrsMain extends BaseDriver {
         System.out.println("test.WrongSearch.getText() = " + e.WrongSearch.getText());
     }
 
+    //Salih MUTLUER
+    @Test
+    public void HastaSilme1(){
+        driver.get("https://openmrs.org/");
+        MyFunc.Bekle(2);
+        WebElement demo= driver.findElement(By.className("zak-button"));
+        demo.click();
+        WebElement buton=driver.findElement(By.xpath("//span[text()='Explore OpenMRS 2']"));
+        wait.until(ExpectedConditions.visibilityOf(buton));
+        buton.click();
+        MyFunc.Bekle(3);
+        WebElement demo2=driver.findElement(By.xpath("//span[text()='Enter the OpenMRS 2 Demo']"));
+        demo2.click();
+        wait.until(ExpectedConditions.urlToBe("https://demo.openmrs.org/openmrs/login.htm"));
+        WebElement user=driver.findElement(By.id("username"));
+        user.sendKeys("Admin");
+        WebElement pass=driver.findElement(By.id("password"));
+        pass.sendKeys("Admin123");
+
+        WebElement isolation= driver.findElement(By.cssSelector("[id='sessionLocation'] :nth-child(2)"));
+        isolation.click();
+        WebElement login= driver.findElement(By.id("loginButton"));
+        login.click();
+        MyFunc.Bekle(2);
+        WebElement record= driver.findElement(By.className("icon-search"));
+        record.click();
+
+        WebElement patient= driver.findElement(By.id("patient-search"));
+        patient.sendKeys("100T5T");
+        MyFunc.Bekle(2);
+        WebElement person= driver.findElement(By.xpath("//td[text()='100T5T']"));
+        person.click();
+        WebElement delete= driver.findElement(By.id("org.openmrs.module.coreapps.deletePatient"));
+        delete.click();
+        WebElement text= driver.findElement(By.id("delete-reason"));
+        text.sendKeys("YES");
+        WebElement confirm= driver.findElement(By.xpath("(//*[@class='confirm right'])[6]"));
+        confirm.click();
+
+    }
+    @Test
+    public void Test2(){
+        driver.get("https://openmrs.org/");
+        MyFunc.Bekle(2);
+        WebElement demo= driver.findElement(By.className("zak-button"));
+        demo.click();
+        WebElement buton=driver.findElement(By.xpath("//span[text()='Explore OpenMRS 2']"));
+        wait.until(ExpectedConditions.visibilityOf(buton));
+        buton.click();
+        MyFunc.Bekle(3);
+        WebElement demo2=driver.findElement(By.xpath("//span[text()='Enter the OpenMRS 2 Demo']"));
+        demo2.click();
+        wait.until(ExpectedConditions.urlToBe("https://demo.openmrs.org/openmrs/login.htm"));
+        WebElement user=driver.findElement(By.id("username"));
+        user.sendKeys("Admin");
+        WebElement pass=driver.findElement(By.id("password"));
+        pass.sendKeys("Admin123");
+
+        WebElement isolation= driver.findElement(By.cssSelector("[id='sessionLocation'] :nth-child(2)"));
+        isolation.click();
+        WebElement login= driver.findElement(By.id("loginButton"));
+        login.click();
+        MyFunc.Bekle(2);
+        WebElement record= driver.findElement(By.className("icon-search"));
+        record.click();
+
+        WebElement patient= driver.findElement(By.id("patient-search"));
+        patient.sendKeys("100T55");
+        MyFunc.Bekle(2);
+        WebElement msj= driver.findElement(By.cssSelector("[class='dataTables_empty']"));
+        Assert.assertTrue(msj.getText().toLowerCase().contains("no matching"));
+        System.out.println("msj.getText() = " + msj.getText());
+    }
 }
+
+
